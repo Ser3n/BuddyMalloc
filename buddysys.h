1feeaca3383d698d59a0132e1617bbb6f364cbb6
Zak Turner <62480261+Ser3n@users.noreply.github.com>
2025-05-27 21:04:24 +1200

    initial

---
+++ buddysys.h
@@ -0,0 +1,47 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+//   Program Name:  Buddy System Algorithm
+//                  
+//   Description:  Buddy System Algorithm
+//                  
+//   Student name:
+//
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef __BUDDYSYS_H__
+#define __BUDDYSYS_H__
+
+
+#include "auxiliary.h"
+
+extern long long int MEMORYSIZE;
+
+
+typedef unsigned char byte;// shorter, replace cast to (char *) with cast to (byte *)
+
+
+//memory_request = n bytes + sizeof(Node);  // make sure the data fits within a Node 
+
+struct llist { long long int size;   //size of the block (ONLY for data, this size does not consider the Node size! (so it is same as s[k])
+               int alloc;  //0 is free, 1 means allocated
+               struct llist * next; //next component
+               struct llist * previous; //previous component
+};
+
+
+typedef struct llist Node;
+
+extern Node *wholememory;
+
+///////////////////////////////////////////////////////////////////////////////////
+
+void *buddyMalloc(int request_memory); 
+int buddyFree(void *p);
+
+
+
+
+
+#endif
